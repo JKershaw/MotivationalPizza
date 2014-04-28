181d070385983287539588f3f4352fe487f8f5a4
Sausage <wardrox@gmail.com>
2014-04-28 10:22:17 +0100

    refactoring

--- routes/home-page.js
+++ routes/home-page.js
@@ -1,6 +1,10 @@
+var Query = require("../lib/Query");
+
 module.exports = function (app) {
 
 	app.get('/', function (request, response) {
+
+		var query = new Query();
 
 		model = {
 			info: request.query.info || false,
