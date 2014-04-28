68f1d05d289efef94fc496775f85146318e01c01
Sausage <wardrox@gmail.com>
2014-04-28 10:16:18 +0100

    refactoring

--- routes/home-page.js
+++ routes/home-page.js
@@ -1,8 +1,15 @@
 module.exports = function (app) {
+
 	app.get('/', function (request, response) {
+
 		model = {
-			info: request.query.info || false
+			info: request.query.info || false,
+			tasks: [{
+				text: "Do the washing up"
+			}]
 		}
+
 		response.render("home-page.ejs", model);
+
 	});
 };
