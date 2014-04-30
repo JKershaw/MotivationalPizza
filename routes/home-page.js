39547cb99237df9f4aab6060cffcc12b9b884524
Sausage <wardrox@gmail.com>
2014-04-30 19:43:49 +0100

    refactoring

--- routes/home-page.js
+++ routes/home-page.js
@@ -1,62 +1,13 @@
-var TaskQuery = require("../lib/TaskQuery");
+var HomePageModelFactory = require("../lib/modelFactories/HomePageModelFactory");
 
 module.exports = function (app) {
 
 	app.get('/', function (request, response) {
 
-		var taskQuery = new TaskQuery();
+		var homePageModelFactory = new HomePageModelFactory();
 
-		taskQuery.allWithStatus("open", function (openTasks) {
-			taskQuery.allWithStatus("done", function (doneTasks) {
-				taskQuery.allWithStatus("not-today", function (notTodayTasks) {
-					taskQuery.doneToday(function (doneTodayTasks) {
-						taskQuery.doneBeforeToday(function (doneBeforeTodayTasks) {
-
-							var infoBox = false;
-
-							switch (request.query.info) {
-
-							case "task-added":
-								infoBox = {
-									class: "alert-info",
-									text: "Your new task has been added."
-								};
-								break;
-							case "task-updated":
-								infoBox = {
-									class: "alert-info",
-									text: "Your task has been updated."
-								};
-								break;
-							case "task-deleted":
-								infoBox = {
-									class: "alert-warning",
-									text: "Your task has been deleted."
-								};
-								break;
-							case "task-done":
-								infoBox = {
-									class: "alert-success",
-									text: "Your task has been done."
-								};
-								break;
-
-							}
-
-							model = {
-								infoBox: infoBox,
-								openTasks: openTasks,
-								doneTodayTasks: doneTodayTasks,
-								doneBeforeTodayTasks: doneBeforeTodayTasks,
-								notTodayTasks: notTodayTasks
-							};
-
-							response.render("home-page.ejs", model);
-						});
-					});
-				});
-			});
+		homePageModelFactory.build(request.query.info, function (model) {
+			response.render("home-page.ejs", model);
 		});
-
 	});
 };
