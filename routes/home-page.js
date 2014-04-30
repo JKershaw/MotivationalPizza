07d67699fb664f35df57c89625198a9d47d7ec7c
Sausage <wardrox@gmail.com>
2014-04-30 12:25:25 +0100

    refactoring

--- routes/home-page.js
+++ routes/home-page.js
@@ -10,34 +10,29 @@ module.exports = function (app) {
 			taskQuery.allWithStatus("done", function (doneTasks) {
 				taskQuery.allWithStatus("not-today", function (notTodayTasks) {
 
-					var info = false,
-						infoBox = false;
+					var infoBox = false;
 
 					switch (request.query.info) {
 
 					case "task-added":
-						info = "task-added";
 						infoBox = {
 							class: "alert-info",
 							text: "Your new task has been added."
 						};
 						break;
 					case "task-updated":
-						info = "task-updated";
 						infoBox = {
 							class: "alert-info",
 							text: "Your task has been updated."
 						};
 						break;
 					case "task-deleted":
-						info = "task-deleted";
 						infoBox = {
 							class: "alert-warning",
 							text: "Your task has been deleted."
 						};
 						break;
 					case "task-done":
-						info = "task-done";
 						infoBox = {
 							class: "alert-success",
 							text: "Your task has been done."
@@ -47,7 +42,6 @@ module.exports = function (app) {
 					}
 
 					model = {
-						info: info,
 						infoBox: infoBox,
 						openTasks: openTasks,
 						doneTasks: doneTasks,
