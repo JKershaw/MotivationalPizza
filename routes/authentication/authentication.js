c83f86c9ba5c5c815ab2e5aced96002adf36afd8
Sausage <wardrox@gmail.com>
2014-05-05 11:20:50 +0100

    Can log in

--- routes/authentication/authentication.js
+++ routes/authentication/authentication.js
@@ -11,8 +11,11 @@ module.exports = function (app, passport) {
 		});
 	});
 
-	// process the login form
-	// app.post('/login', do all our passport stuff here);
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
 
 	app.get('/signup', function (request, response) {
 
