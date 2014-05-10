c2fe5ad55b5f92a6c539e39483ca717e6d23f83e
Sausage <wardrox@gmail.com>
2014-05-10 15:19:47 +0100

    refactoring

--- tests/unit/today_cutoff_timestamp_generator.js
+++ tests/unit/today_cutoff_timestamp_generator.js
@@ -1,18 +1,15 @@
 var assert = require('assert'),
-	TaskQuery = require("../../lib/TaskQuery");
+	DailyCutoffCalculator = require('../../lib/util/DailyCutoffCalculator');
 
 test("Given a timestamp of now, return the timestamp for 3am this morning", function (done) {
 
-	var fakeRequest = {
-		user: false
-	},
-		taskQuery = new TaskQuery(fakeRequest);
+	var dailyCutoffCalculator = new DailyCutoffCalculator();
 		
 	cutoffTimestamp = Date.today().add({
 		hours: 3
 	}).getTime();
 
-	var returnedCutoffTimestamp = taskQuery.calulateDailyCutoff();
+	var returnedCutoffTimestamp = dailyCutoffCalculator.calculate();
 
 	assert.equal(returnedCutoffTimestamp, cutoffTimestamp);
 	done();
