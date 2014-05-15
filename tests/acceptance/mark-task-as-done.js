dafba9a20ea6c2f8054dc976ce1fc04876421d17
Sausage <wardrox@gmail.com>
2014-05-15 10:33:45 +0100

    UI updated to allow editing of done tasks

--- tests/acceptance/mark-task-as-done.js
+++ tests/acceptance/mark-task-as-done.js
@@ -58,6 +58,26 @@ describe('Given I sign up', function (done) {
 				it("Then I am told I have no tasks to do and can have pizza", function () {
 					expect(browser.text('#no-current-tasks')).to.contain("Hurray! You've done everything you need to today. Pizza for dinner!");
 				});
+
+				describe("And I click the delete task button for this", function (done) {
+					before(function (done) {
+						browser.clickLink('.task .delete-task', done);
+					});
+					describe("And I click Yes I'm sure", function (done) {
+						before(function (done) {
+							browser.clickLink('.task .delete-task-yes', done);
+						});
+
+						it("Then I can no longer see the task listed", function () {
+							expect(browser.text('.task')).to.not.contain(existingTasks[0].text);
+						});
+
+						it("And I am told the task has been deleted", function () {
+							expect(browser.text('#info-box')).to.contain("Your task has been deleted");
+						});
+					});
+
+				});
 			});
 		});
 	});
