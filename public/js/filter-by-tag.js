e6373dc9098a206d6db22d7d956ea98ceb6e60eb
Sausage <wardrox@gmail.com>
2014-05-16 12:23:18 +0100

    moving filtering into main view

--- public/js/filter-by-tag.js
+++ public/js/filter-by-tag.js
@@ -2,14 +2,14 @@ $('.tag-filter').on('click', function () {
 
 	if ($(this).data('tag-clear')) {
 
-		$(".task").show();
+		$(".task-not-today").show();
 
 	} else {
 
 		var tagValue = $(this).data('tag-value');
 
-		$(".task").hide();
-		$(".task[data-tags*='" + tagValue + "']").show();
+		$(".task-not-today").hide();
+		$(".task-not-today[data-tags*='" + tagValue + "']").show();
 	}
 
 });
