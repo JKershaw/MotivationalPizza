976eed4f06f947592b59e977a6e009dde8c2990a
Sausage <wardrox@gmail.com>
2014-05-16 19:50:06 +0100

    refactoring

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,11 @@
 var PageRenderer = require("../lib/util/PageRenderer"),
-	User = require("../lib/authentication/User");
+	User = require("../lib/authentication/User"),
+	UsersRepository = require('../lib/repositories/UsersRepository');
 
 module.exports = function (app) {
 
-	var user = new User();
+	var usersRepository = new UsersRepository(),
+		user = new User(usersRepository);
 
 	app.get('/profile', function (request, response) {
 
--- routes/sms.js
+++ routes/sms.js
@@ -1,10 +1,12 @@
 var SmsHandler = require("../lib/util/SmsHandler"),
-	TasksRepository = require('../lib/repositories/TasksRepository');
+	TasksRepository = require('../lib/repositories/TasksRepository'),
+	UsersRepository = require('../lib/repositories/UsersRepository');
 
 module.exports = function (app) {
 
 	var tasksRepository = new TasksRepository(),
-		smsHandler = new SmsHandler(tasksRepository);
+		usersRepository = new UsersRepository(),
+		smsHandler = new SmsHandler(tasksRepository, usersRepository);
 
 	app.all('/SMS', function (request, response) {
 		smsHandler.handle(request.body, response);
