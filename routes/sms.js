70c9dba9badfbd7609644dacbf6fe679a92a3861
Sausage <wardrox@gmail.com>
2014-05-16 19:17:17 +0100

    can send an SMS and save a task

--- routes/sms.js
+++ routes/sms.js
@@ -1,28 +1,38 @@
+var User = require("../lib/authentication/User"),
+	MpApp = require('../lib/app/MpApp'),
+	TaskObjectFactory = require('../lib/objectFactories/TaskObjectFactory'),
+	TasksRepository = require('../lib/repositories/TasksRepository');
+
 module.exports = function (app) {
 
-	app.all('/SMS', function (request, response) {
-		console.log("Body: ", request.body);
-		response.send(200);
-	});
+	var user = new User(),
+		tasksRepository = new TasksRepository(),
+		mpApp = new MpApp(tasksRepository),
+		taskObjectFactory = new TaskObjectFactory();
 
-	var fake_request_body = {
-		ToCountry: 'GB',
-		ToState: 'Colchester',
-		SmsMessageSid: 'SM48996417d2cc3a63a6d4f715fdbe36b3',
-		NumMedia: '0',
-		ToCity: '',
-		FromZip: '',
-		SmsSid: 'SM48996417d2cc3a63a6d4f715fdbe36b3',
-		FromState: '',
-		SmsStatus: 'received',
-		FromCity: '',
-		Body: 'Again!',
-		FromCountry: 'GB',
-		To: '+441206704037',
-		ToZip: '',
-		MessageSid: 'SM48996417d2cc3a63a6d4f715fdbe36b3',
-		AccountSid: 'AC3334c6a5fc4fb198c9d5cb96fe9fb9b5',
-		From: '+447976048640',
-		ApiVersion: '2010-04-01'
-	};
+	app.all('/SMS', function (request, response) {
+
+		user.findByPhoneNumber(request.body.From, function (actualUser) {
+
+			console.log("Text message recieved: ", request.body);
+			console.log("User: ", actualUser);
+
+			if (!actualUser) {
+				return response.send("Sorry, we didn't recognise your phone number.");
+			}
+
+			var userId = actualUser._id,
+				newTask = {
+					text: request.body.Body,
+					tagsString: "SMS"
+				};
+
+			taskObjectFactory.build(newTask, userId, function (task) {
+				mpApp.command.add(task, function (success) {
+					response.send(200);
+				});
+			});
+
+		});
+	});
 };
