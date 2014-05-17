83438b8dc9d4ac2df4001749154ec917912b4bcb
Sausage <wardrox@gmail.com>
2014-05-17 16:15:11 +0100

    adding unit tests for if today is full specification

---
+++ tests/unit/today_full_specification_tests.js
@@ -0,0 +1,128 @@
+var assert = require('assert'),
+	TodayFullSpecification = require("../../lib/app/commands/specifications/TodayFullSpecification");
+
+test("Given a user has no preference and 1 task exists for today, return false", function (done) {
+
+	var tasksRepository = new FakeTasksRepository([{}]),
+		usersRepository = new FakeUsersRepository({}),
+		todayFullSpecification = new TodayFullSpecification(tasksRepository, usersRepository);
+
+	var userId = 1;
+
+	todayFullSpecification.isSatisfiedBy(userId, function (todayIsFull) {
+
+		assert.equal(todayIsFull, false);
+		done();
+
+	});
+});
+
+test("Given a user has no preference and 5 tasks exists for today, return true", function (done) {
+
+	var tasksRepository = new FakeTasksRepository([{}, {}, {}, {}, {}]),
+		usersRepository = new FakeUsersRepository({}),
+		todayFullSpecification = new TodayFullSpecification(tasksRepository, usersRepository);
+
+	var userId = 2;
+
+	todayFullSpecification.isSatisfiedBy(userId, function (todayIsFull) {
+
+		assert.equal(todayIsFull, true);
+		done();
+
+	});
+});
+
+test("Given a user a preference for 10 today days max, and 5 tasks exists for today, return false", function (done) {
+
+	var tasksRepository = new FakeTasksRepository([{}, {}, {}, {}, {}]),
+		usersRepository = new FakeUsersRepository({
+			maxTodayTaskCount: 10
+		}),
+		todayFullSpecification = new TodayFullSpecification(tasksRepository, usersRepository);
+
+	var userId = 2;
+
+	todayFullSpecification.isSatisfiedBy(userId, function (todayIsFull) {
+
+		assert.equal(todayIsFull, false);
+		done();
+
+	});
+});
+
+test("Given a user a preference for 10 today days max, and 10 tasks exists for today, return true", function (done) {
+
+	var tasksRepository = new FakeTasksRepository([{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]),
+		usersRepository = new FakeUsersRepository({
+			maxTodayTaskCount: 10
+		}),
+		todayFullSpecification = new TodayFullSpecification(tasksRepository, usersRepository);
+
+	var userId = 2;
+
+	todayFullSpecification.isSatisfiedBy(userId, function (todayIsFull) {
+
+		assert.equal(todayIsFull, true);
+		done();
+
+	});
+});
+
+test("Given a user a preference for 3 today days max, and 2 tasks exists for today, return false", function (done) {
+
+	var tasksRepository = new FakeTasksRepository([{}, {}]),
+		usersRepository = new FakeUsersRepository({
+			maxTodayTaskCount: 3
+		}),
+		todayFullSpecification = new TodayFullSpecification(tasksRepository, usersRepository);
+
+	var userId = 2;
+
+	todayFullSpecification.isSatisfiedBy(userId, function (todayIsFull) {
+
+		assert.equal(todayIsFull, false);
+		done();
+
+	});
+});
+
+test("Given a user a preference for 3 today days max, and 3 tasks exists for today, return true", function (done) {
+
+	var tasksRepository = new FakeTasksRepository([{}, {}, {}]),
+		usersRepository = new FakeUsersRepository({
+			maxTodayTaskCount: 3
+		}),
+		todayFullSpecification = new TodayFullSpecification(tasksRepository, usersRepository);
+
+	var userId = 2;
+
+	todayFullSpecification.isSatisfiedBy(userId, function (todayIsFull) {
+
+		assert.equal(todayIsFull, true);
+		done();
+
+	});
+});
+
+var FakeTasksRepository = function (findResults) {
+
+	function find(query, callback) {
+		callback(findResults);
+	}
+
+	return {
+		find: find
+	};
+};
+
+var FakeUsersRepository = function (findResults) {
+
+	function findById(userId, callback) {
+		callback(findResults);
+	}
+
+	return {
+		findById: findById
+	};
+};
