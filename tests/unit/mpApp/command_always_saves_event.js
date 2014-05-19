7e8243c0373f0c3875af1479dff89c32b5cce575
Sausage <wardrox@gmail.com>
2014-05-19 12:05:45 +0100

    refactoring

--- tests/unit/mpApp/command_always_saves_event.js
+++ tests/unit/mpApp/command_always_saves_event.js
@@ -1,144 +1,6 @@
 var assert = require('assert'),
 	MpAppCommand = require('../../../lib/app/Command');
 
-var newTask = {
-	user: "1",
-	status: "open"
-};
-
-var eventStoreUsed, eventTypeSaved;
-
-
-test("When I add a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.add(newTask, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskAdd");
-		done();
-	});
-});
-
-test("When I remove a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.remove(newTask, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskRemove");
-		done();
-	});
-});
-
-test("When I markAsDone a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.markAsDone(newTask, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskMarkAsDone");
-		done();
-	});
-});
-
-test("When I markAsToday a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.markAsToday(newTask, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskMarkAsToday");
-		done();
-	});
-});
-
-test("When I markAsNotToday a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.markAsNotToday(newTask, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskMarkAsSomeOtherTime");
-		done();
-	});
-});
-
-test("When I markAsTomorrow a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.markAsTomorrow(newTask, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskMarkAsTomorrow");
-		done();
-	});
-});
-
-test("When I updateText a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.updateText(newTask, "some text", function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskUpdateText");
-		done();
-	});
-});
-
-test("When I bump a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.bump(newTask, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskBump");
-		done();
-	});
-});
-
-test("When I addTags a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.addTags(newTask, [], function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "TaskAddTags");
-		done();
-	});
-});
-
-test("When I saveUser a task, the event store is used", function (done) {
-
-	var mpAppCommand = new MpAppCommand(new FakeEventStore(), new Date(), new FakeRepository(), new FakeRepository());
-	eventStoreUsed = false;
-	eventTypeSaved = false;
-
-	mpAppCommand.saveUser({}, function (success) {
-		assert.equal(eventStoreUsed, true);
-		assert.equal(eventTypeSaved, "UserAdd");
-		done();
-	});
-});
-
 var FakeEventStore = function () {
 
 	function save(eventType, eventData, callback) {
@@ -152,38 +14,163 @@ var FakeEventStore = function () {
 	};
 };
 
-var FakeRepository = function () {
-
-	function save(object, callback) {
-		callback(true);
-	}
-
-	function update(query, updateRequest, callback) {
-		callback(true);
-	}
-
-	function remove(object, callback) {
-		callback(true);
-	}
-
-	function find(query, callback) {
-		callback([{}]);
-	}
-
-	function findById(query, callback) {
-		callback([{}]);
-	}
-
-	function buildQuery() {
-		return {};
-	}
-
+var FakeModule = function() {
 	return {
-		save: save,
-		find: find,
-		findById: findById,
-		buildQuery: buildQuery,
-		update: update,
-		remove: remove
+		run: function (args, callback) {
+			callback();
+		}
 	};
 };
+
+var FakeCommandModuleLoader = function () {
+
+	function load(name) {
+		return new FakeModule();
+	}
+	return {
+		load: load
+	};
+};
+
+var newTask = {
+	user: "1",
+	status: "open"
+};
+
+var eventStoreUsed, eventTypeSaved;
+
+var mpAppCommand = new MpAppCommand(new FakeEventStore(), null, null, new FakeCommandModuleLoader());
+
+test("When I run a command, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	var commandName = "TaskAdd";
+
+	mpAppCommand.run(commandName, {task: newTask}, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, commandName);
+		done();
+	});
+});
+
+test("When I add a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.add(newTask, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskAdd");
+		done();
+	});
+});
+
+test("When I remove a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.remove(newTask, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskRemove");
+		done();
+	});
+});
+
+test("When I markAsDone a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.markAsDone(newTask, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskMarkAsDone");
+		done();
+	});
+});
+
+test("When I markAsToday a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.markAsToday(newTask, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskMarkAsToday");
+		done();
+	});
+});
+
+test("When I markAsNotToday a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.markAsNotToday(newTask, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskMarkAsSomeOtherTime");
+		done();
+	});
+});
+
+test("When I markAsTomorrow a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.markAsTomorrow(newTask, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskMarkAsTomorrow");
+		done();
+	});
+});
+
+test("When I updateText a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.updateText(newTask, "some text", function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskUpdateText");
+		done();
+	});
+});
+
+test("When I bump a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.bump(newTask, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskBump");
+		done();
+	});
+});
+
+test("When I addTags a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.addTags(newTask, [], function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "TaskAddTags");
+		done();
+	});
+});
+
+test("When I saveUser a task, the event store is used", function (done) {
+
+	eventStoreUsed = false;
+	eventTypeSaved = false;
+
+	mpAppCommand.saveUser({}, function (success) {
+		assert.equal(eventStoreUsed, true);
+		assert.equal(eventTypeSaved, "UserAdd");
+		done();
+	});
+});
