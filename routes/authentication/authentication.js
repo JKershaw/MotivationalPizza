cf10461c0c4bf5b1e605fd0183ed17ef12984439
Sausage <wardrox@gmail.com>
2014-09-07 12:17:50 +0100

    refactoring out app

--- routes/authentication/authentication.js
+++ routes/authentication/authentication.js
@@ -38,14 +38,3 @@ module.exports = function (app, passport) {
 		response.redirect('/');
 	});
 };
-
-// route middleware to make sure a user is logged in
-function isLoggedIn(request, response, next) {
-
-	// if user is authenticated in the session, carry on 
-	if (request.isAuthenticated())
-		return next();
-
-	// if they aren't redirect them to the home page
-	response.redirect('/');
-}
