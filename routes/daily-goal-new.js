0e7a10be9dee47b889b1951ecf49f18d724b74d2
Sausage <wardrox@gmail.com>
2014-09-09 12:26:35 +0100

    refactoring to save task in db

--- routes/daily-goal-new.js
+++ routes/daily-goal-new.js
@@ -1,11 +1,23 @@
 var HomePageViewModelFactory = require("../lib/modelViewFactories/HomePageViewModelFactory"),
-	PageRenderer = require("../lib/util/PageRenderer");
+	PageRenderer = require("../lib/util/PageRenderer"),
+	Repository = require("../lib/repositories/Repository");
 
 module.exports = function (app) {
 
 	app.post('/newGoalForToday', function (request, response) {
 
-		response.redirect("/");
+		var dailyGoalsRepository = new Repository("dailyGoals");
 
+		var newDailyGoal = {
+			text: request.body.newDailyGoalName,
+			color: request.body.newDailyTaskColour,
+			created_at: new Date().getTime(),
+			complete: false,
+			user_id: request.user._id
+		};
+
+		dailyGoalsRepository.save(newDailyGoal, function(){
+			response.redirect("/");
+		});
 	});
 };
